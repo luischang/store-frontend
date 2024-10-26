010ab1a939551804f9054ac4ac76679fb6158d40
Luis Chang <luis.chang@outlook.com>
2024-10-25 21:41:17 -0500

    Muestras de quasar

--- src/components/auth/LoginForm.vue
+++ src/components/auth/LoginForm.vue
@@ -1,6 +1,22 @@
 <template>
-    <h5>Este es el componente Login</h5>
-</template>
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          :text="['hey, how are you?']"
+          sent
+        />
+        <q-chat-message
+          :text="['doing fine, how r you?']"
+        />
+      </div>
+    </div>
+    <q-rating
+        v-model="ratingModel"
+        size="1.5em"
+        icon="thumb_up"
+        color="red-7"
+      />
+  </template>
 
 <style>
 
